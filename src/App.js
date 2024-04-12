cd49684abe56e02084516b6b5b35bd72b975194e
Ivory Wang <iywang@cs.washington.edu>
2024-04-11 17:55:48 -0700

    modified:   src/App.js modified:   src/auth.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         />
       </React.StrictMode>
     );
-  }
+  };
 
   const SongMatchClick = () => {
   };
--- src/auth.js
+++ src/auth.js
@@ -11,3 +11,64 @@ const sha256 = async (plain) => {
     const data = encoder.encode(plain)
     return window.crypto.subtle.digest('SHA-256', data)
 }
+
+const base64encode = (input) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+}
+
+const hashed = await sha256(codeVerifier)
+const codeChallenge = base64encode(hashed);
+
+const clientId = 'YOUR_CLIENT_ID';
+const redirectUri = 'http://localhost:8080';
+
+const scope = 'user-read-private user-read-email';
+const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+// generated in the previous step
+window.localStorage.setItem('code_verifier', codeVerifier);
+
+const params =  {
+  response_type: 'code',
+  client_id: clientId,
+  scope,
+  code_challenge_method: 'S256',
+  code_challenge: codeChallenge,
+  redirect_uri: redirectUri,
+}
+
+authUrl.search = new URLSearchParams(params).toString();
+window.location.href = authUrl.toString();
+
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+const getToken = async code => {
+
+    // stored in the previous step
+    let codeVerifier = localStorage.getItem('code_verifier');
+  
+    const payload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+      }),
+    }
+  
+    const body = await fetch(url, payload);
+    const response =await body.json();
+  
+    localStorage.setItem('access_token', response.access_token);
+}
+
+export const access_token = response.access_token;
