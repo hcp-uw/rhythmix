7785f630991528868de4a33474503ea60effe532
Ivory Wang <iywang@cs.washington.edu>
2024-04-11 17:45:49 -0700

    modified:   src/CustomPlaylist/index.css modified:   src/auth.js

--- src/auth.js
+++ src/auth.js
@@ -5,3 +5,9 @@ const generateRandomString = (length) => {
 }
 
 const codeVerifier  = generateRandomString(64);
+
+const sha256 = async (plain) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+}
