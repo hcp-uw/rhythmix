3606308a6abd936ef82b806e229fc09e7ed6632f
William Ong <wlo12489@gmail.com>
2024-04-16 19:18:04 -0700

    Added logo for tab and home page - changed background color

--- src/DiscoverDaily/DiscoverDaily.tsx
+++ src/DiscoverDaily/DiscoverDaily.tsx
@@ -1,8 +1,11 @@
 export const UpdatePlaylists = () => {
+    // Check when a playlist was last updated
+    // If made 1 day ago (yesterday), loop through every playlist, remove every song
+    // Find 25 songs for each playlist, algo needed:
     console.log(Date());
 }
 
-export const GenreToPlaylistMap= new Map<string, string>([
+export const GenreToPlaylistMap = new Map<string, string>([
     ["Pop", "https://open.spotify.com/embed/playlist/37i9dQZF1EQncLwOalG3K7?utm_source=generator"],
     ["Hip-Hop", "https://open.spotify.com/embed/playlist/37i9dQZF1EQnqst5TRi17F?utm_source=generator"],
     ["Indie", "https://open.spotify.com/embed/playlist/37i9dQZF1EQqkOPvHGajmW?utm_source=generator"],
