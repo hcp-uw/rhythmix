0b46c8fe5f3061e9d505aa1922f66197b06768d6
noahhoang <noahhoang591@gmail.com>
2024-05-03 01:04:10 -0700

    Updated SongMatch SearchBar
    Added some front end to the searchbar function for SongMatch

--- src/SongMatch/SongMatch.tsx
+++ src/SongMatch/SongMatch.tsx
@@ -1,4 +1,5 @@
 import React, { Component, ChangeEvent, MouseEvent } from "react";
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
 import { Root } from "react-dom/client";
 import './index.css';
 import SearchBar from "./SearchBar";
@@ -21,7 +22,7 @@ type SongMatchState = {
     currentSearch: string;
     currentPage: "home" | "searchbar" | "songlist" | "recommendations" | "playlist";
     errorMessage: string;
-    song: string;
+    songResults: any[]; // current list of diplayed songs
 
     // match pool content
     
@@ -38,7 +39,7 @@ export class SongMatch extends Component<SongMatchProps, SongMatchState> {
             currentSearch: "test",
             currentPage: "home",
             errorMessage: "",
-            song: ""
+            songResults: []
             
             
             
@@ -63,6 +64,12 @@ export class SongMatch extends Component<SongMatchProps, SongMatchState> {
                 <SearchBar/>
             )
 
+        } else if (this.state.currentPage === "songlist") {
+            
+            return(
+                <div/>
+            )
+            
         } else {
             return (
                 <div>other page</div>
@@ -75,19 +82,48 @@ export class SongMatch extends Component<SongMatchProps, SongMatchState> {
 
     renderStartPage = (): JSX.Element => {
         return (
-            <div>
-                <label>
-                Search Song:
-                <input type="text" value={this.state.currentSearch} onChange={this.doSearchChange}></input>
-                </label>
-                <button onClick={this.doSearchClick}>Search</button>
-                
-                <button onClick={this.doSearchBarClick}>SEARCH BAR TYPE SHIT</button>
+            <div className="App">
+                <Container>
+                    <InputGroup className="mb-3" size="lg">
+                    <FormControl
+                        placeholder="Search for Artist"
+                        type="input"
+                        onKeyUp={event => {
+                        if (event.key === "Enter") {
+                            this.searchSong();
+                        }
+                        }}
+                        onChange={this.doSearchChange}
+                    />
+                    <Button onClick={() => this.searchSong()}>
+                        Search
+                    </Button>
 
-                <br/>
-                <button onClick={this.doBackClick}>Back</button>
-            </div>
-        );
+                    <Button onClick={() => this.doBackClick}>
+                        Back
+                    </Button>
+                    </InputGroup>
+                </Container>
+
+                <Container>
+                    <Row className="mx-2 row row-cols-6">
+                    
+                    {this.state.songResults.map( (song, i) => {
+                        return (
+                        <Card>
+                            <Card.Img src={song.images[0].url}/>
+                            <Card.Body>
+                            <Card.Title>{song.name}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        )
+                    })}
+            
+                    </Row>
+                </Container>
+
+                </div>
+        )
     }
 
 
@@ -124,10 +160,49 @@ export class SongMatch extends Component<SongMatchProps, SongMatchState> {
             this.setState({errorMessage: "Please enter a Song in the Search Bar"});
 
         } else {    // searchbar is not empty, SEARCH
-
-
-
+            // run the api call
+            this.searchSong();
         }
+
+    }
+
+
+    searchSong = async (): Promise<void> => {
+        const CLIENT_ID = "e910cd42af954cd39b2e04cb4a1a43c3";
+        const CLIENT_SECRET = "2e5b8f0e3f464084bd3546d5dad312c5";
+
+        // API Access Token
+        var authParameters = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+        }
+        // call to get token, and update value
+        const result = await fetch('https://accounts.spotify.com/api/token', authParameters);
+        const data = await result.json();
+        const accessToken = data.access_token;
+
+        // GET request using Search to get Artist ID
+        var searchParameters = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            }
+        }
+        var artistID = await fetch('https://api.spotify.com/v1/search?q=' + this.state.currentSearch + '&type=artist', searchParameters)
+            .then(response => response.json())
+            .then(data => { return data.artists.items[0].id }); // saves artist id to artistID - gets most popular result
+
+        // GET request with Artist ID, grab all albums from artist
+        var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=10', searchParameters)
+            .then(response => response.json())
+            .then(data => { 
+                console.log(data);
+                this.setState({songResults: data.items});  // stores the 50 albums as an array of records
+            });
 
     }
 
