bcb65bbbf9032a6c0f34f1b26eeef5a80b4a10ca
noahhoang <noahhoang591@gmail.com>
2024-05-14 01:28:49 -0700

    Updated SearchBar to display songs
    changed spotify query parameters to search for songs rather than albums

--- src/SongMatch/SongMatch.tsx
+++ src/SongMatch/SongMatch.tsx
@@ -39,7 +39,7 @@ export class SongMatch extends Component<SongMatchProps, SongMatchState> {
             currentSearch: "test",
             currentPage: "home",
             errorMessage: "",
-            songResults: []
+            songResults: [],
             
             
             
@@ -103,7 +103,7 @@ export class SongMatch extends Component<SongMatchProps, SongMatchState> {
                     </InputGroup>
                 </Container>
 
-                <Container>
+                {/* <Container>
                     <Row className="mx-2 row row-cols-6">
                     
                     {this.state.songResults.map( (song, i) => {
@@ -118,7 +118,25 @@ export class SongMatch extends Component<SongMatchProps, SongMatchState> {
                     })}
             
                     </Row>
+                </Container> */}
+
+                <Container>
+                    <Row className="mx-2 row row-cols-6">
+                    
+                    {this.state.songResults.map( (song, i) => {
+                        return (
+                        <Card>
+                            <Card.Img src={song.album.images[0].url}/>
+                            <Card.Body>
+                            <Card.Title>{song.name}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        )
+                    })}
+            
+                    </Row>
                 </Container>
+
 
                 </div>
         )
@@ -165,6 +183,46 @@ export class SongMatch extends Component<SongMatchProps, SongMatchState> {
     }
 
 
+    // searchSong = async (): Promise<void> => {
+    //     const CLIENT_ID = "e910cd42af954cd39b2e04cb4a1a43c3";
+    //     const CLIENT_SECRET = "2e5b8f0e3f464084bd3546d5dad312c5";
+
+    //     // API Access Token
+    //     var authParameters = {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/x-www-form-urlencoded'
+    //         },
+    //         body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    //     }
+    //     // call to get token, and update value
+    //     const result = await fetch('https://accounts.spotify.com/api/token', authParameters);
+    //     const data = await result.json();
+    //     const accessToken = data.access_token;
+
+    //     // GET request using Search to get Artist ID
+    //     var searchParameters = {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Authorization': 'Bearer ' + accessToken
+    //         }
+    //     }
+    //     var artistID = await fetch('https://api.spotify.com/v1/search?q=' + this.state.currentSearch + '&type=artist', searchParameters)
+    //         .then(response => response.json())
+    //         .then(data => { return data.artists.items[0].id }); // saves artist id to artistID - gets most popular result
+
+    //     // GET request with Artist ID, grab all albums from artist
+    //     var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=10', searchParameters)
+    //         .then(response => response.json())
+    //         .then(data => { 
+    //             console.log(data);
+    //             this.setState({songResults: data.items});  // stores the 50 albums as an array of records
+    //         });
+
+    // }
+
+
     searchSong = async (): Promise<void> => {
         const CLIENT_ID = "e910cd42af954cd39b2e04cb4a1a43c3";
         const CLIENT_SECRET = "2e5b8f0e3f464084bd3546d5dad312c5";
@@ -190,17 +248,10 @@ export class SongMatch extends Component<SongMatchProps, SongMatchState> {
                 'Authorization': 'Bearer ' + accessToken
             }
         }
-        var artistID = await fetch('https://api.spotify.com/v1/search?q=' + this.state.currentSearch + '&type=artist', searchParameters)
+        var songID = await fetch('https://api.spotify.com/v1/search?q=' + this.state.currentSearch + '&type=track&limit=10', searchParameters)
             .then(response => response.json())
-            .then(data => { return data.artists.items[0].id }); // saves artist id to artistID - gets most popular result
-
-        // GET request with Artist ID, grab all albums from artist
-        var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=10', searchParameters)
-            .then(response => response.json())
-            .then(data => { 
-                console.log(data);
-                this.setState({songResults: data.items});  // stores the 50 albums as an array of records
-            });
+            // .then(data => console.log(data))  // FOR QUERY TESTING
+            .then(data => {this.setState({songResults: data.tracks.items})})
 
     }
 
