8beec1fa51f4b146786e056b6f81243e76f6a225
william_ong <wlo12489@gmail.com>
2024-09-11 02:25:31 -0700

    Fixed App rendering warnings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom/client';
 import { HomePage } from './HomePage.tsx';
 
 function App() {
-  const main = ReactDOM.createRoot(document.getElementById("main"));
-  main.render(
-    <React.StrictMode>
-      <HomePage />
-    </React.StrictMode>
-  )
+  return (
+    <HomePage />
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-export const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('main'));
 root.render(
   <React.StrictMode>
     <App />
