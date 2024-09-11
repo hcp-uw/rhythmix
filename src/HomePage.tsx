9e002601e01c704aa4a9f8c9a8b319ca51b09290
william_ong <wlo12489@gmail.com>
2024-09-10 20:48:21 -0700

    Added more playlist genres

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -7,12 +7,16 @@ import { loginWithSpotifyClick, logoutClick } from "./spotify.js";
 
 export const accessTokenGLOBAL = localStorage.getItem('access_token');
 
-// TODO: add more genres
 const genreToPlaylistMap = new Map<string, string>([
-  ["Pop", "https://open.spotify.com/embed/playlist/2sTcvjcZasAQwlgDrVprbD?utm_source=generator"],
-  ["Hip-Hop", "https://open.spotify.com/embed/playlist/2AF0jOomrpvwo81QdCiTB9?utm_source=generator"],
-  ["Indie", "https://open.spotify.com/embed/playlist/0aLJwR85YiVpuf2Udmn6Ti?utm_source=generator"],
-  ["R&B", "https://open.spotify.com/embed/playlist/0osjfZWfYKvLd7RtsIqu4T?utm_source=generator"],
+  ["Pop", "https://open.spotify.com/embed/playlist/6WZx6JODAeplGmQL7YBXfK?utm_source=generator"],
+  ["Hip-Hop", "https://open.spotify.com/embed/playlist/5n9846Hwm6OsDicJ5a0v6f?utm_source=generator"],
+  ["Indie", "https://open.spotify.com/embed/playlist/1JHl3qS08VD8Qcb0pfqGU3?utm_source=generator"],
+  ["R&B", "https://open.spotify.com/embed/playlist/2ooZlUQuH5xNgrJkD1dpe5?utm_source=generator"],
+  ["EDM", "https://open.spotify.com/embed/playlist/0T0JF0dPzzczmQ3hV9x2q5?utm_source=generator"],
+  ["Romance", "https://open.spotify.com/embed/playlist/6vGCq5o22Q2o351Nim1TLJ?utm_source=generator"],
+  ["K-pop", "https://open.spotify.com/embed/playlist/37A4pqUHFQwR1GqNZI8qII?utm_source=generator"],
+  ["Alternative", "https://open.spotify.com/embed/playlist/4Hlej1tVLz2XU6RtChIurM?utm_source=generator"],
+  ["Workout", "https://open.spotify.com/embed/playlist/5W8GJqJ4GaJ3NkEOrjTRSf?utm_source=generator"]
 ]);
 
 type HomePageState = {
@@ -88,10 +92,17 @@ export class HomePage extends Component<null, HomePageState> {
                   onChange={this.doGenreChange}
                   className="styled-select"
                 >
-                  <option value="Pop">Pop</option>
-                  <option value="Hip-Hop">Hip-Hop</option>
-                  <option value="Indie">Indie</option>
-                  <option value="R&B">R&B</option>
+
+                <option value="Alternative">Alternative</option>
+                <option value="EDM">EDM</option>
+                <option value="Hip-Hop">Hip-Hop</option>
+                <option value="Indie">Indie</option>
+                <option value="K-pop">K-pop</option>
+                <option value="Pop">Pop</option>
+                <option value="R&B">R&B</option>
+                <option value="Romance">Romance</option>
+                <option value="Workout">Workout</option>
+              
                 </select>
 
                 <div>
