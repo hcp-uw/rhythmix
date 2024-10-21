1f2ed9aa3783882d5fb6b3750c6be9b9aa3557db
William Ong <wlo12489@gmail.com>
2024-10-21 14:21:35 -0700

    Added secure playlist refresh

--- netlify/functions/updatePlaylists.js
+++ netlify/functions/updatePlaylists.js
@@ -1,9 +1,16 @@
 const { updatePlaylists } = require('./functions');
 
 exports.handler = async (event, context) => {
-  await updatePlaylists();
+  const key = event.path.split('/').pop()
+  if (key === process.env.REFRESH_ACCESS_KEY) {
+    await updatePlaylists();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Updated playlists"}),
+    };
+  }
   return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Updated playlists"}),
+    statusCode: 500,
+    body: JSON.stringify("Incorrect key"),
   };
 };
